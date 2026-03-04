311ea9b3f1529b627f0d0cb58dfacba18a0a1b9e
maceo <lapaludmaceo@gmail.com>
2026-03-04 14:40:55 +0100

    liste des liens pour la nav pas encore op, rectif banner

--- kasa/src/components/header.jsx
+++ kasa/src/components/header.jsx
@@ -1,9 +1,11 @@
 import logo from "./LOGO.png";
+import "../styles/header.scss";
 
 function Header() {
   return (
-    <header style={{ padding: "20px", background: "white", color: "white" }}>
+    <header style={{ background: "white", color: "black" }}>
       <img src={logo} alt="Logo Kasa" className="header__logo" />
+      <ul><li><a href="">Accueil</a></li><li><a href="">A Propos</a></li></ul>
     </header>
   );
 }
