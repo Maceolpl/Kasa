e37308ec9db5a659fc49d54b83265a925f60417c
maceo <lapaludmaceo@gmail.com>
2026-04-03 11:11:57 +0200

    correction des collapses page logement + responsive logement et about

--- kasa/src/pages/logement.jsx
+++ kasa/src/pages/logement.jsx
@@ -28,27 +28,25 @@ function Logement() {
           </ul>
         </div>
 
-        <div className="logement__host">
-          <span className="logement__host-name">{host.name}</span>
-          <img src={host.picture} alt={host.name} className="logement__host-img" />
+        <div className="logement__host-rating">
+          <div className="logement__host">
+            <span className="logement__host-name">{host.name}</span>
+            <img src={host.picture} alt={host.name} className="logement__host-img" />
+          </div>
+          <div className="logement__rating">
+            {[1, 2, 3, 4, 5].map((star) => (
+              <span key={star} className={`logement__star ${star <= Number(rating) ? "active" : ""}`}>
+                ★
+              </span>
+            ))}
+          </div>
         </div>
-      </div>
-
-      <div className="logement__rating">
-        {[1, 2, 3, 4, 5].map((star) => (
-          <span
-            key={star}
-            className={`logement__star ${star <= Number(rating) ? "active" : ""}`}
-          >
-            ★
-          </span>
-        ))}
       </div>
 
       <div className="logement__collapses">
         <Collapse title="Description">{description}</Collapse>
         <Collapse title="Équipements">
-          <ul>
+          <ul className="logement__equipments">
             {equipments.map((eq) => (
               <li key={eq}>{eq}</li>
             ))}
